88eff7156d91e1cde0c958adc7c5f3098763da90
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-06-26 11:59:31 +0200

    [UP] helper of view home

--- AwesomeProject/src/Views/Home/Home.js
+++ AwesomeProject/src/Views/Home/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {View, Text, Button, Alert} from 'react-native';
-
+import {SafeAreaView, View, Text, Button} from 'react-native';
+import stylesHome from './Home.style';
+import styles from '../index.style';
+import onPress from './HomeManager';
 const Home = ({navigation}) => {
   return (
-    <View>
-      <Text>Home</Text>
-      <Button onPress={() => onPress(navigation)} title="Go To Page" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>Home</Text>
+        <Text style={stylesHome.textInHome}>
+          This text is just in Home view
+        </Text>
+        <Button onPress={() => onPress(navigation)} title="Go To Page" />
+      </View>
+    </SafeAreaView>
   );
 };
-
-function onPress(navigation) {
-  Alert.alert('ciao');
-  navigation.navigate('Profile');
-}
-
 export default Home;
---
+++ AwesomeProject/src/Views/index.style.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+});

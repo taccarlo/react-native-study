921c3e0da6c739717ce3f17152725c459430cd4f
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-06-26 12:58:51 +0200

    [UP] img

--- AwesomeProject/src/Models/Constants/images.js
+++ AwesomeProject/src/Models/Constants/images.js
@@ -1,5 +1,4 @@
-const GTSA_logo =
-  'https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png'; //require('../../../assets/GTSA-logo.png');
+const GTSA_logo = require('../../../assets/GTSA-logo.png');
 
 export default {
   GTSA_logo,
--- AwesomeProject/src/Views/Profile/Profile.js
+++ AwesomeProject/src/Views/Profile/Profile.js
@@ -3,16 +3,17 @@ import React from 'react';
 import {Image, View, Text} from 'react-native';
 
 const Profile = () => {
-  let Image_Http_URL = {
-    uri: images.GTSA_logo, //'https://reactnativecode.com/wp-content/uploads/2017/05/react_thumb_install.png',
-  };
   return (
-    <View>
+    <View style={{flexDirection: 'row'}}>
       <Text>Profile</Text>
 
       <Image
-        source={Image_Http_URL}
-        style={{height: 200, resizeMode: 'stretch', margin: 5}}
+        source={require('../../../assets/GTSA-logo.png')} //images.GTSA_logo}
+        style={{
+          margin: 5,
+          resizeMode: 'stretch',
+          height: 200,
+        }}
       />
     </View>
   );
